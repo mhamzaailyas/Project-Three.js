48f0c90e42990d50b2e59cadc9065b89b146c9a8
mhamzaailyas <mhamzaa1947@egmail.com>
2024-07-05 13:58:41 +0500

    Startup Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Environment } from '@react-three/drei'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Canvas>
+      <ambientLight intensity={0.5 * Math.PI} />
+      <Sphere scale={1.75} />
+      <OrbitControls />
+      <Environment preset="city" />
+    </Canvas>
+  )
 }
 
-export default App;
+function Sphere(props) {
+  return (
+    <mesh {...props}>
+      <sphereGeometry args={[1, 32, 32]} />
+      <meshStandardMaterial color="grey" />
+    </mesh>
+  )
+}
